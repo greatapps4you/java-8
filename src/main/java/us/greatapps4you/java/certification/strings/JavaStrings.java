01971a150c134d07d38df22d68a13fb5e99a24de
Jose E. de Souza N <developer@greatapps4you.us>
2020-12-14 11:31:03 +0100

    [java-certification] Updated Copyright

---
+++ src/main/java/us/greatapps4you/java/certification/strings/JavaStrings.java
@@ -0,0 +1,10 @@
+/*
+ * GreatApps4you LLC Copyright (c) 2020
+ * https://greatapps4you.us
+ * CSSML NDSMD VRS + NSMV SMQL IVB
+ */
+
+package us.greatapps4you.java.certification.strings;
+
+public class JavaStrings {
+}
